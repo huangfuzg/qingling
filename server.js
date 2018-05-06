d7c9f1ec20a33b3fd5cf9ff08adb5ec045e094be
seiger <15594818350@163.com>
2018-05-06 20:41:05 +0800

    test

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/line',function(req,res){
 app.get('/product',function(req,res){
     res.sendFile(__dirname+'/'+"p.html");
 })
+app.get('/news',function(req,res){
+    res.sendFile(__dirname+'/'+"news.html");
+})
 
 var server = app.listen(8094,'0.0.0.0',function(){
     var host = server.address().address
